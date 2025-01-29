96f425ff929ec24d366501f1e6945049ca2613df
Iman <mashrapov2005@gmail.com>
2025-01-29 15:03:49 +0600

    The first commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from person import Person
 
-person1 = Person('Ruslan', 'Asanov', student_id=1)
-person2 = Person('Aman', 'Talgatov', student_id=1)
-person3 = Person('Sultan', 'Bakytbek uulu', student_id=1)
+if __name__ == "__main__":
+    person1 = Person('Ruslan', 'Asanov', student_id=1)
+    person2 = Person('Aman', 'Talgatov', student_id=1)
+    person3 = Person('Sultan', 'Bakytbek uulu', student_id=1)
+    person4 = Person('Damir', 'Imanaliev', student_id=1)
 
-lst = [person1, person2, person3]
+    lst = [person1, person2, person3, person4]
 
 
-for x in lst:
-    print(x.walk())
+    for x in lst:
+        print(x.walk())
 
